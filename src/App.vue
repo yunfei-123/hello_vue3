d642d50005970e1553e686ac5b075dd858a87ddf
huyunfei <314939779@qq.com>
2025-07-05 17:22:47 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app">
+    <h2>你你你</h2>
+    <button onclick="changeName">123</button>
+    <button onclick="changeAge">456</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "App1",
+};
+</script>
+
+<script lang="ts">
+function changeName() {}
+function changeAge() {}
+</script>
+
+<style>
+.app {
+  background-color: aqua;
+  padding: 10px;
+  box-shadow: 1 1 6px;
+  border-radius: 2px;
+}
+</style>
